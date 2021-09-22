41bef08ecea879b979d2ae9b02a28ee3db64d070
Abhishec177 <abhishek122k@gmail.com>
2021-09-22 14:55:20 +0530

    contract initialization

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomiclabs/hardhat-waffle");
+
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
+
+projectId = "06cf09f52faf48c681eccbba26e12c3d";
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
+  solidity: "0.8.4",
+};
